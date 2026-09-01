8e155cfc5a5a5628d2b8a2242794744726926dfc
Maelnsn <maelnisin@hotmail.be>
2026-09-01 19:45:47 +0200

    je comprend mieux

--- devboard-cli/src/index.ts
+++ devboard-cli/src/index.ts
@@ -1,2 +1,10 @@
-let message: string = "DevBoard Démarre";
-console.log(message);
+const developerName: string = "Maël";
+let city: string = "Charneux";
+let available: boolean = true;
+let weeklyHours: number = 40;
+let currentYear: number = new Date().getFullYear();
+let startYear: number = 2002;
+let experiencesYears: number = currentYear - startYear;
+
+console.log(`Salut je m'appel ${developerName}. Je vis à ${city}, j'ai ${experiencesYears} années d'expérience. Je suis en ce moment ${available ? "disponible" : "non disponible"} pour travailler ${weeklyHours} heures par semaines.`);
+//Merci l'autocomplétion pour le gain de temps et de compréhension.
