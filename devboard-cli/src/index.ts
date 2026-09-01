5cc1ab8f1721d05e756696b581db460f209f0435
Maelnsn <maelnisin@hotmail.be>
2026-09-01 20:25:18 +0200

    push it

--- devboard-cli/src/index.ts
+++ devboard-cli/src/index.ts
@@ -6,5 +6,12 @@ let currentYear: number = new Date().getFullYear();
 let startYear: number = 2002;
 let experiencesYears: number = currentYear - startYear;
 
-console.log(`Salut je m'appel ${developerName}. Je vis à ${city}, j'ai ${experiencesYears} années d'expérience. Je suis en ce moment ${available ? "disponible" : "non disponible"} pour travailler ${weeklyHours} heures par semaines.`);
+//console.log(`Salut je m'appel ${developerName}. Je vis à ${city}, j'ai ${experiencesYears} années d'expérience. Je suis en ce moment ${available ? "disponible" : "non disponible"} pour travailler ${weeklyHours} heures par semaines.`);
 //Merci l'autocomplétion pour le gain de temps et de compréhension.
+
+//--------------------------------------------------------------------------------------------------
+
+const technologies: string[] = ["Typescript", "Node.js", "Git"];
+technologies.push("PostgreSQL");
+//console.log(`Technologies: (${technologies.length}) ${technologies.join(" , ")}`);
+console.table(technologies);
