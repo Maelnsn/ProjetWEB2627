2185d405bf5decfecad746cb0185ea44e97e41ae
Maelnsn <maelnisin@hotmail.be>
2026-09-01 21:01:57 +0200

    oui

--- devboard-cli/src/index.ts
+++ devboard-cli/src/index.ts
@@ -7,6 +7,7 @@ let startYear: number = 2002;
 let experiencesYears: number = currentYear - startYear;
 
 //console.log(`Salut je m'appel ${developerName}. Je vis à ${city}, j'ai ${experiencesYears} années d'expérience. Je suis en ce moment ${available ? "disponible" : "non disponible"} pour travailler ${weeklyHours} heures par semaines.`);
+
 //Merci l'autocomplétion pour le gain de temps et de compréhension.
 
 //--------------------------------------------------------------------------------------------------
@@ -14,4 +15,37 @@ let experiencesYears: number = currentYear - startYear;
 const technologies: string[] = ["Typescript", "Node.js", "Git"];
 technologies.push("PostgreSQL");
 //console.log(`Technologies: (${technologies.length}) ${technologies.join(" , ")}`);
-console.table(technologies);
+//console.table(technologies);
+
+//-------------------------------------------------------------------------------------------------
+
+interface DevProfile {
+
+    name: string;
+    city: string;
+    experiencesYears: number;
+    available: boolean;
+    weeklyHours: number;
+    technologies: string[];
+}
+
+const moi: DevProfile = {
+    name: "Maël",
+    city: "Charneux",
+    experiencesYears: 21,
+    available: true,
+    weeklyHours: 4,
+    technologies: ["Typescript", "Node.js", "Git", "PostgreSQL"]
+};
+
+moi.name = developerName;
+moi.city = city;
+moi.experiencesYears = experiencesYears;
+moi.available = available;
+moi.weeklyHours = weeklyHours;
+moi.technologies = technologies;
+
+console.log(`Salut je m'appel ${moi.name}. Je vis à ${moi.city}, j'ai ${moi.experiencesYears} années d'expérience. Je suis en ce moment ${moi.available ? "disponible" : "non disponible"} pour travailler ${moi.weeklyHours} heures par semaines.`);
+
+//-------------------------------------------------------------------------------------------------
+
